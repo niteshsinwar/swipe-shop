5c84413b8ec182fe16fc9b3cdcc3e679150b4a26
niteshsinwar <niteshsinwar@gmail.com>
2025-03-27 23:11:15 +0530

    improved design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route,Navigate } from 'react-router-dom';
 import { LikedProductsProvider } from './context/LikedProductsContext';
 import Home from './pages/Home';
 import Store from './pages/Store';
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/store" element={<Store />} />
           <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </LikedProductsProvider>
